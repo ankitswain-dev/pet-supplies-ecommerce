daf9efa2d592153406d52c0ed5b3404b3d034326
ankitswain-dev <coderankit51@gmail.com>
2025-09-15 23:39:38 +0530

    Fix import case sensitivity for Products file

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import products from "../data/products";
+import products from "../data/Products";
 import { useCart } from "../context/CartContext";
 import { addToCart } from "../services/cartService";
 import { motion } from "framer-motion";
